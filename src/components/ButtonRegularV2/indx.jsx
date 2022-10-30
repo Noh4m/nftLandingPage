0c85a869ecd5840a541d72ee53d1b11e6b7a6992
Noham <72859612+Noh4m@users.noreply.github.com>
2022-10-31 00:12:06 +0100

    fix name

--- src/components/ButtonRegularV2/indx.jsx
+++ src/components/ButtonRegularV2/indx.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function ButtonRegular(props) {
+export default function ButtonRegularV2(props) {
   return (
     <button className={` ${props.class}  lg:text-lg md:text-xs font-Poppins font-semibold border-white border-solid border-2 p-2  rounded-[60px] hover:bg-regal-blue-hover md:block `}>
         {props.children}
